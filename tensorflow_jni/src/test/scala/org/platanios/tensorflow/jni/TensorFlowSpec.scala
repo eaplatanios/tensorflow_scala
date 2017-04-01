3b687d3d86fcce84ef9250beba4a9afd2bab79e5
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-04-01 14:31:31 -0400

    Minor bug fix in the TensorFlow native library specification.

--- tensorflow_jni/src/test/scala/org/platanios/tensorflow/jni/TensorFlowSpec.scala
+++ tensorflow_jni/src/test/scala/org/platanios/tensorflow/jni/TensorFlowSpec.scala
@@ -1,16 +1,12 @@
 package org.platanios.tensorflow.jni
 
 import org.scalatest._
-import org.tensorflow.framework.OpList
-
-import scala.collection.JavaConverters._
 
 /**
   * @author Emmanouil Antonios Platanios
   */
 class TensorFlowSpec extends FlatSpec {
   "The TensorFlow library version" should "have non-zero length" in {
-    val opList = OpList.parseFrom(Operation.allOps).getOpList.asScala
     assert(TensorFlow.version.length > 0)
   }
 }
