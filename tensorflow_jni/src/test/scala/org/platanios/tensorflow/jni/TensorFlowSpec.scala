9cfe18fe3e2a8298efea38bb5eddc22d69e4bcb3
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-04-10 03:31:56 -0400

    Minor addition to the TensorFlow native library specification.

--- tensorflow_jni/src/test/scala/org/platanios/tensorflow/jni/TensorFlowSpec.scala
+++ tensorflow_jni/src/test/scala/org/platanios/tensorflow/jni/TensorFlowSpec.scala
@@ -11,6 +11,8 @@ class TensorFlowSpec extends FlatSpec {
   }
 
   "The TensorFlow library data type sizes" must "be correct" in {
+    assert(TensorFlow.dataTypeSize(-1) === 0)
+    assert(TensorFlow.dataTypeSize(0) === 0)
     assert(TensorFlow.dataTypeSize(1) === 4)
     assert(TensorFlow.dataTypeSize(2) === 8)
     assert(TensorFlow.dataTypeSize(3) === 4)
@@ -31,5 +33,27 @@ class TensorFlowSpec extends FlatSpec {
     assert(TensorFlow.dataTypeSize(18) === 16)
     assert(TensorFlow.dataTypeSize(19) === 2)
     assert(TensorFlow.dataTypeSize(20) === 0)
+    assert(TensorFlow.dataTypeSize(101) === 0)
+    assert(TensorFlow.dataTypeSize(102) === 0)
+    assert(TensorFlow.dataTypeSize(103) === 0)
+    assert(TensorFlow.dataTypeSize(104) === 0)
+    assert(TensorFlow.dataTypeSize(105) === 0)
+    assert(TensorFlow.dataTypeSize(106) === 0)
+    assert(TensorFlow.dataTypeSize(107) === 0)
+    assert(TensorFlow.dataTypeSize(108) === 0)
+    assert(TensorFlow.dataTypeSize(109) === 0)
+    assert(TensorFlow.dataTypeSize(110) === 0)
+    assert(TensorFlow.dataTypeSize(111) === 0)
+    assert(TensorFlow.dataTypeSize(112) === 0)
+    assert(TensorFlow.dataTypeSize(113) === 0)
+    assert(TensorFlow.dataTypeSize(114) === 0)
+    assert(TensorFlow.dataTypeSize(115) === 0)
+    assert(TensorFlow.dataTypeSize(116) === 0)
+    assert(TensorFlow.dataTypeSize(117) === 0)
+    assert(TensorFlow.dataTypeSize(118) === 0)
+    assert(TensorFlow.dataTypeSize(119) === 0)
+    assert(TensorFlow.dataTypeSize(120) === 0)
+    assert(TensorFlow.dataTypeSize(121) === 0)
+    assert(TensorFlow.dataTypeSize(167) === 0)
   }
 }
