923c980375038ddbb911755667c0f275c99b9196
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-05-18 20:10:10 -0400

    Added Apache 2.0 license.

--- examples/src/main/scala/org/platanios/tensorflow/examples/LinearRegression.scala
+++ examples/src/main/scala/org/platanios/tensorflow/examples/LinearRegression.scala
@@ -1,3 +1,17 @@
+// Copyright 2017, Emmanouil Antonios Platanios. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 package org.platanios.tensorflow.examples
 
 import org.platanios.tensorflow.api._
@@ -27,14 +41,6 @@ object LinearRegression {
     val predictions = tf.matMul(inputs, weights)
     val loss = tf.sum(tf.square(predictions - outputs))
     val trainOp = optimizer.minimize(loss)
-
-    val testOp = tf.placeholder(tf.FLOAT32, tf.Shape(-1, 34, 65))
-    val reshapedTestOp = tf.reshape(testOp, tf.Shape(-1, 1, 65, 2, 17))
-
-    val graphDef = tf.defaultGraph.toGraphDef
-    val graph1 = tf.Graph()
-    graph1.importGraphDef(graphDef)
-    val graph0 = tf.defaultGraph
 
     logger.info("Training the linear regression model.")
     val session = tf.Session()
--- tensorflow/src/main/scala/org/platanios/tensorflow/jni/Tensor.scala
+++ tensorflow/src/main/scala/org/platanios/tensorflow/jni/Tensor.scala
@@ -1,3 +1,17 @@
+// Copyright 2017, Emmanouil Antonios Platanios. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
 package org.platanios.tensorflow.jni
 
 import java.nio.ByteBuffer
@@ -16,15 +30,4 @@ object Tensor {
   @native def getEncodedStringSize(numStringBytes: Int): Int
   @native def setStringBytes(stringBytes: Array[Byte], buffer: ByteBuffer): Int
   @native def getStringBytes(buffer: ByteBuffer): Array[Byte]
-
-  //  @native def allocate(dataType: Int, shape: Array[Long], byteSize: Long): Long
-  //  @native def allocateScalarBytes(value: Array[Byte]): Long
-  //  @native def setValue(handle: Long, value: Any): Unit
-  //  @native def scalarFloat(handle: Long): Float
-  //  @native def scalarDouble(handle: Long): Double
-  //  @native def scalarInt(handle: Long): Int
-  //  @native def scalarLong(handle: Long): Long
-  //  @native def scalarBoolean(handle: Long): Boolean
-  //  @native def scalarBytes(handle: Long): Array[Byte]
-  //  @native def readNDArray(handle: Long, value: Any): Unit
 }
