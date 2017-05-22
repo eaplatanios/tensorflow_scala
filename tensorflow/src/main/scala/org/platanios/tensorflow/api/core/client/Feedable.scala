52b2f814b97015db3d7542a12702b09b5dab5341
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-05-22 02:21:38 -0400

    Converted "Feedable" to a type class.

--- tensorflow/src/main/scala/org/platanios/tensorflow/api/core/client/Feedable.scala
+++ tensorflow/src/main/scala/org/platanios/tensorflow/api/core/client/Feedable.scala
@@ -18,24 +18,77 @@ package org.platanios.tensorflow.api.core.client
 import org.platanios.tensorflow.api.ops.Op
 import org.platanios.tensorflow.api.tensors.Tensor
 
-/**
+/** Feedables can be fed into a TensorFlow session to fix the values of certain tensors to the provided values.
+  *
+  * For example, values for placeholder ops can be fed into TensorFlow sessions.
+  *
+  * Currently supported feedable types are:
+  *   - [[Op.Output]] whose fed value should be a single [[Tensor]].
+  *   - [[Op.OutputIndexedSlices]] whose fed value should be tuple [[(Tensor, Tensor, Tensor)]].
+  *   - [[Op.SparseOutput]] whose fed value should be tuple [[(Tensor, Tensor, Tensor)]].
+  *
+  * Feedables are fed into TensorFlow sessions through the use of [[FeedMap]]s. Any [[Map]] that uses a feedable type as
+  * the keys type and its corresponding value type as its values type is a valid feed map.
+  *
   * @author Emmanouil Antonios Platanios
   */
 trait Feedable[T] {
-  def toFeedMap(value: T): Map[Op.Output, Tensor]
+  type ValueType
+  def feed(feedable: T, value: ValueType): Map[Op.Output, Tensor]
 }
 
-case class FeedMap(values: Map[Op.Output, Tensor] = Map.empty) {
-  def feed[T](feedable: Feedable[T], value: T): FeedMap = {
-    FeedMap(values ++ feedable.toFeedMap(value))
+object Feedable {
+  type Aux[T, V] = Feedable[T] {type ValueType = V}
+
+  def apply[T, V](implicit ev: Aux[T, V]): Aux[T, V] = ev
+
+  implicit val opOutputFeedable: Aux[Op.Output, Tensor] = new Feedable[Op.Output] {
+    override type ValueType = Tensor
+    override def feed(feedable: Op.Output, value: ValueType): Map[Op.Output, Tensor] = Map(feedable -> value)
+  }
+
+  // TODO: [TENSORS] Switch to something like "TensorIndexedSlices".
+  implicit val opOutputIndexedSlicesFeedable: Aux[Op.OutputIndexedSlices, (Tensor, Tensor, Tensor)] = {
+    new Feedable[Op.OutputIndexedSlices] {
+      override type ValueType = (Tensor, Tensor, Tensor)
+      override def feed(feedable: Op.OutputIndexedSlices, value: ValueType): Map[Op.Output, Tensor] = {
+        Map(feedable.indices -> value._1, feedable.values -> value._2, feedable.denseShape -> value._3)
+      }
+    }
+  }
+
+  // TODO: [TENSORS] Switch to something like "SparseTensor".
+  implicit val opSparseOutputFeedable: Aux[Op.SparseOutput, (Tensor, Tensor, Tensor)] = {
+    new Feedable[Op.SparseOutput] {
+      override type ValueType = (Tensor, Tensor, Tensor)
+      override def feed(feedable: Op.SparseOutput, value: ValueType): Map[Op.Output, Tensor] = {
+        Map(feedable.indices -> value._1, feedable.values -> value._2, feedable.denseShape -> value._3)
+      }
+    }
   }
 }
 
+/** Represents TensorFlow feed maps for sessions.
+  *
+  * @param  values Map from tensors in a graph to their values.
+  */
+class FeedMap private[client] (val values: Map[Op.Output, Tensor] = Map.empty) {
+  def feed[T, V](feedable: T, value: V)(implicit ev: Feedable.Aux[T, V]): FeedMap = {
+    FeedMap(values ++ ev.feed(feedable, value))
+  }
+
+  def +(other: FeedMap): FeedMap = FeedMap(values ++ other.values)
+}
+
 object FeedMap {
-  val empty = FeedMap()
+  def apply(values: Map[Op.Output, Tensor]): FeedMap = new FeedMap(values)
+
+  val empty = new FeedMap()
 
   trait Implicits {
-    implicit def feedMap(feeds: Map[Op.Output, Tensor]): FeedMap = FeedMap(feeds)
+    implicit def feedMap[T, V](feeds: Map[T, V])(implicit ev: Feedable.Aux[T, V]): FeedMap = {
+      FeedMap(feeds.toSeq.map(f => ev.feed(f._1, f._2)).reduce(_ ++ _))
+    }
   }
 
   object Implicits extends Implicits
--- tensorflow/src/test/scala/org/platanios/tensorflow/api/core/client/FeedableSuite.scala
+++ tensorflow/src/test/scala/org/platanios/tensorflow/api/core/client/FeedableSuite.scala
@@ -19,7 +19,6 @@ import org.platanios.tensorflow.api._
 import org.platanios.tensorflow.api.core.Graph
 import org.platanios.tensorflow.api.ops.{Basic, Op}
 import org.platanios.tensorflow.api.tensors.Tensor
-import org.platanios.tensorflow.api.types.FLOAT32
 
 import org.scalatest.junit.JUnitSuite
 import org.junit.Test
@@ -28,29 +27,40 @@ import org.junit.Test
   * @author Emmanouil Antonios Platanios
   */
 class FeedableSuite extends JUnitSuite {
-  case class DummyFeedable() extends Feedable[Tensor] {
-    val feed: Op.Output = Basic.placeholder(FLOAT32)
+  def feedMapIdentity(feedMap: FeedMap): FeedMap = feedMap
 
-    override def toFeedMap(value: Tensor): Map[Op.Output, Tensor] = Map(feed -> value)
-  }
-
-  case class DummyFeedable2() extends Feedable[(Tensor, Tensor)] {
-    val feed1: Op.Output = Basic.placeholder(FLOAT32)
-    val feed2: Op.Output = Basic.placeholder(FLOAT32)
-
-    override def toFeedMap(value: (Tensor, Tensor)): Map[Op.Output, Tensor] = {
-      Map(feed1 -> value._1, feed2 -> value._2)
+  @Test def testFeedMap(): Unit = using(Graph()) { graph =>
+    Op.createWith(graph) {
+      val tensor0 = Tensor(0.0f)
+      val tensor1 = Tensor(1.0f)
+      val tensor2 = Tensor(2.0f)
+      val tensor3 = Tensor(3.0f)
+      val feedable1 = Basic.placeholder(tf.FLOAT32)
+      val feedable2 = Op.OutputIndexedSlices(
+        Basic.placeholder(tf.FLOAT32), Basic.placeholder(tf.FLOAT32), Basic.placeholder(tf.FLOAT32))
+      val feedable1FeedMap = feedMapIdentity(Map(feedable1 -> tensor0))
+      val feedable2FeedMap = feedMapIdentity(Map(feedable2 -> (tensor1, tensor2, tensor3)))
+      assert(feedable1FeedMap.values === Map(feedable1 -> tensor0))
+      assert(feedable2FeedMap.values ===
+                 Map(feedable2.indices -> tensor1, feedable2.values -> tensor2, feedable2.denseShape -> tensor3))
     }
   }
 
-  @Test def testFeedable(): Unit = using(Graph()) { graph =>
+  @Test def testHeterogeneousFeedMap(): Unit = using(Graph()) { graph =>
     Op.createWith(graph) {
-      val feedable1 = DummyFeedable()
-      val feedable2 = DummyFeedable2()
-      val feedable1FeedMap = feedable1.toFeedMap(Tensor(0))
-      val feedable2FeedMap = feedable2.toFeedMap(Tensor(1), Tensor(2))
-      assert(feedable1FeedMap === Map(feedable1.feed -> Tensor(0)))
-      assert(feedable2FeedMap === Map(feedable2.feed1 -> Tensor(1), feedable2.feed2 -> Tensor(2)))
+      val tensor0 = Tensor(0.0f)
+      val tensor1 = Tensor(1.0f)
+      val tensor2 = Tensor(2.0f)
+      val tensor3 = Tensor(3.0f)
+      val feedable1 = Basic.placeholder(tf.FLOAT32)
+      val feedable2 = Op.OutputIndexedSlices(
+        Basic.placeholder(tf.FLOAT32), Basic.placeholder(tf.FLOAT32), Basic.placeholder(tf.FLOAT32))
+      val feedable1FeedMap: FeedMap = feedMapIdentity(Map(feedable1 -> tensor0))
+      val feedable2FeedMap: FeedMap = feedMapIdentity(Map(feedable2 -> (tensor1, tensor2, tensor3)))
+      val feedMap = feedMapIdentity(feedable1FeedMap + feedable2FeedMap)
+      assert(feedMap.values === Map(
+        feedable1 -> tensor0,
+        feedable2.indices -> tensor1, feedable2.values -> tensor2, feedable2.denseShape -> tensor3))
     }
   }
 }
