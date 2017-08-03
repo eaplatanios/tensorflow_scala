b95c1c01836a8462ff907fc8f81a14b8db88a607
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-08-03 19:40:30 -0400

    Improved the feedable type trait to add support for recursive feed data structures.

--- api/src/main/scala/org/platanios/tensorflow/api/core/client/Feedable.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/core/client/Feedable.scala
@@ -18,6 +18,11 @@ package org.platanios.tensorflow.api.core.client
 import org.platanios.tensorflow.api.ops.{Output, OutputIndexedSlices, SparseOutput}
 import org.platanios.tensorflow.api.tensors.Tensor
 
+import shapeless.{::, Generic, HList, HNil}
+
+import scala.collection.SeqLike
+import scala.language.higherKinds
+
 /** Feedables can be fed into a TensorFlow session to fix the values of certain tensors to the provided values.
   *
   * For example, values for placeholder ops can be fed into TensorFlow sessions.
@@ -66,6 +71,54 @@ object Feedable {
       }
     }
   }
+
+  implicit def feedableSeq[T, V, CC[A] <: SeqLike[A, CC[A]]](implicit ev: Aux[T, V]): Aux[CC[T], CC[V]] = {
+    new Feedable[CC[T]] {
+      override type ValueType = CC[V]
+      override def feed(feedable: CC[T], value: CC[V]): Map[Output, Tensor] = {
+        feedable.toSeq.zip(value.toSeq).foldLeft(Map.empty[Output, Tensor])({
+          case (feedMap, pair) => feedMap ++ ev.feed(pair._1, pair._2)
+        })
+      }
+    }
+  }
+
+  implicit def feedableArray[T, V](implicit ev: Aux[T, V]): Aux[Array[T], Array[V]] = {
+    new Feedable[Array[T]] {
+      override type ValueType = Array[V]
+      override def feed(feedable: Array[T], value: Array[V]): Map[Output, Tensor] = {
+        feedable.toSeq.zip(value.toSeq).foldLeft(Map.empty[Output, Tensor])({
+          case (feedMap, pair) => feedMap ++ ev.feed(pair._1, pair._2)
+        })
+      }
+    }
+  }
+
+  // Feedable maps are intentionally not allowed because they would be inefficient without any real good use cases.
+
+  implicit val hnil: Aux[HNil, HNil] = new Feedable[HNil] {
+    override type ValueType = HNil
+    override def feed(feedable: HNil, value: HNil): Map[Output, Tensor] = Map.empty[Output, Tensor]
+  }
+
+  implicit def recursiveConstructor[H, R, T <: HList, TO <: HList](implicit
+      feedableHead: Aux[H, R],
+      feedableTail: Aux[T, TO]
+  ): Aux[H :: T, R :: TO] = new Feedable[H :: T] {
+    override type ValueType = R :: TO
+    override def feed(feedable: H :: T, value: R :: TO): Map[Output, Tensor] = {
+      feedableHead.feed(feedable.head, value.head) ++ feedableTail.feed(feedable.tail, value.tail)
+    }
+  }
+
+  implicit def productConstructor[P <: Product, L <: HList, LO <: HList, R](implicit
+      genP: Generic.Aux[P, L],
+      genR: Generic.Aux[R, LO],
+      feedbaleL: Aux[L, LO]
+  ): Aux[P, R] = new Feedable[P] {
+    override type ValueType = R
+    override def feed(feedable: P, value: R): Map[Output, Tensor] = feedbaleL.feed(genP.to(feedable), genR.to(value))
+  }
 }
 
 /** Represents TensorFlow feed maps for sessions.
@@ -88,6 +141,10 @@ object FeedMap {
   val empty = new FeedMap()
 
   trait Implicits {
+    implicit def feedMap[T, V](feeds: (T, V)*)(implicit ev: Feedable.Aux[T, V]): FeedMap = {
+      FeedMap(feeds.flatMap { case (k, v) => ev.feed(k, v) }.toMap)
+    }
+
     implicit def feedMap[T, V](feeds: Map[T, V])(implicit ev: Feedable.Aux[T, V]): FeedMap = {
       FeedMap(feeds.flatMap { case (k, v) => ev.feed(k, v) })
     }
