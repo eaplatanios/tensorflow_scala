fec9b1b4d186de5c71999442df84b02b52600043
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-09-15 20:48:25 -0400

    Made some updates to the JVM callback op.

--- jni/src/main/native/ops/jvm_callback_op.cc
+++ jni/src/main/native/ops/jvm_callback_op.cc
@@ -1,79 +1,49 @@
-/* Copyright 2017, Emmanouil Antonios Platanios. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
 
-#include "jvm_callback_op.h"
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-#include <iostream>
+    http://www.apache.org/licenses/LICENSE-2.0
 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "tensorflow/core/framework/op_kernel.h"
+
+#include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
+#include "tensorflow/core/kernels/jvm_callback_op.h"
 #include "tensorflow/core/lib/core/coding.h"
 #include "tensorflow/core/lib/core/errors.h"
 #include "tensorflow/core/platform/macros.h"
 
-#include "utilities.h"
+namespace {
+// Copy of the C Eager API struct due to the circular dependency issue.
+TFE_TensorHandle* TFE_NewTensorHandle(const tensorflow::Tensor& t) {
+  return new TFE_TensorHandle(t, nullptr);
+}
 
-typedef struct TF_Status TF_Status;
-typedef struct TFE_TensorHandle TFE_TensorHandle;
-typedef struct JVMCall JVMCall;
+// Copy of the C Eager API struct due to the circular dependency issue.
+const tensorflow::Tensor* TFE_TensorHandleUnderlyingTensorInHostMemory(
+    TFE_TensorHandle* h, TF_Status* status) {
+  if (h->d != nullptr) {
+    status->status = tensorflow::errors::FailedPrecondition(
+        "TFE_TensorHandle is placed in device (not host) memory. Cannot return "
+            "a tensorflow::Tensor");
+    return nullptr;
+  }
+  return &h->t;
+}
+}
 
 namespace tensorflow {
-
-REGISTER_OP("JVMCallback")
-    .Input("input: Tin")
-    .Output("output: Tout")
-    .Attr("id: int")
-    .Attr("jvm_pointer: string")
-    .Attr("registry_class_name: string")
-    .Attr("Tin: list(type) >= 0")
-    .Attr("Tout: list(type) >=0")
-    .SetIsStateful()
-    .SetShapeFn(shape_inference::UnknownShape)
-    .Doc(R"doc(
-Invokes a JVM callback function, `f` to compute `f(input)->output`.
-
-This operation is considered stateful. For a stateless version, see
-`JVMCallback`.
-
-id: A unique ID representing a registered JVM callback function
-  in this address space.
-jvm_pointer: A pointer to an existing JVM instance represented as a
-  string. This is the JVM that will be used when invoking this JVM
-  callback.
-registry_class_name: Name of the callbacks registry class.
-input: List of tensors that will provide input to the op.
-output: Output tensors from the op.
-Tin: Data types of the inputs to the op.
-Tout: Data types of the outputs from the op.
-      The length of the list specifies the number of outputs.
-)doc");
-
-REGISTER_OP("JVMCallbackStateless")
-    .Input("input: Tin")
-    .Output("output: Tout")
-    .Attr("id: int")
-    .Attr("jvm_pointer: string")
-    .Attr("registry_class_name: string")
-    .Attr("Tin: list(type) >= 0")
-    .Attr("Tout: list(type) >= 0")
-    .SetShapeFn(shape_inference::UnknownShape)
-    .Doc(R"doc(
-A stateless version of `JVMCallback`.
-)doc");
-
 namespace {
   // Given the 'call', prepares the inputs as a JNI long array that is appropriate for calling the registry.
   jlongArray MakeInputs(JVMCall* call) {
@@ -95,7 +65,12 @@ namespace {
     jsize n = call->env->GetArrayLength(call_outputs);
     jlong* outputs_array = call->env->GetLongArrayElements(call_outputs, nullptr);
     for (int i = 0; i < n; ++i) {
-      auto* h = require_handle<TFE_TensorHandle>(call->env, outputs_array[i], "output");
+      static_assert(sizeof(jlong) >= sizeof(TFE_TensorHandle*), "Cannot package C object pointers as a Java long");
+      if (outputs_array[i] == 0) {
+        status->status = errors::InvalidArgument("One of the op output tensors has been disposed already.");
+        return;
+      }
+      auto* h = reinterpret_cast<TFE_TensorHandle*>(outputs_array[i]);
       if (h == nullptr) {
         status->status = errors::InvalidArgument("Could not obtain tensor handle to one of the outputs.");
         return;
@@ -114,8 +89,32 @@ namespace {
 
     // Invoke the registry 'call' method.
     auto outputs = (jlongArray) call->env->CallStaticObjectMethod(
-      call->registry, call->call_method_id, call->id, call_inputs);
-    if (env->ExceptionCheck()) return Status::OK();
+        call->registry, call->call_method_id, call->id, call_inputs);
+    if (call->env->ExceptionCheck()) return Status::OK();
+    jthrowable exc = call->env->ExceptionOccurred();
+    if (exc) {
+      jclass classMethodAccess = call->env->FindClass("java/lang/Class");
+      jmethodID classNameMethodID = call->env->GetMethodID(classMethodAccess, "getName", "()Ljava/lang/String;");
+      jstring clsName = (jstring) call->env->CallObjectMethod(exc, classNameMethodID);
+      const char* clsNameCString = call->env->GetStringUTFChars(clsName, NULL);
+      std::string clsNameCppString(clsNameCString);
+      if (clsNameCppString == "java/lang/IllegalArgumentException") {
+        call->env->ReleaseStringUTFChars(clsName, clsNameCString);
+        return errors::InvalidArgument("Failed to run JVM callback function.");
+      } else if (clsNameCppString == "java/lang/SecurityException") {
+        call->env->ReleaseStringUTFChars(clsName, clsNameCString);
+        return errors::PermissionDenied("Failed to run JVM callback function.");
+      } else if (clsNameCppString == "java/lang/IllegalStateException") {
+        call->env->ReleaseStringUTFChars(clsName, clsNameCString);
+        return errors::FailedPrecondition("Failed to run JVM callback function.");
+      } else if (clsNameCppString == "java/lang/IndexOutOfBoundsException") {
+        call->env->ReleaseStringUTFChars(clsName, clsNameCString);
+        return errors::OutOfRange("Failed to run JVM callback function.");
+      } else {
+        call->env->ReleaseStringUTFChars(clsName, clsNameCString);
+        return errors::Unknown("Failed to run JVM callback function.");
+      }
+    }
 
     if (outputs == nullptr) {
       return errors::Unknown("Failed to run JVM callback function.");
@@ -129,19 +128,21 @@ namespace {
 }  // namespace
 
 class JVMCallbackOp : public OpKernel {
- public:
+public:
   explicit JVMCallbackOp(OpKernelConstruction* ctx) : OpKernel(ctx) {
     OP_REQUIRES_OK(ctx, ctx->GetAttr("id", &id_));
     std::string jvm_pointer;
     OP_REQUIRES_OK(ctx, ctx->GetAttr("jvm_pointer", &jvm_pointer));
     jvm_ = pointerFromString<JavaVM*>(jvm_pointer);
     JNIEnv* env;
-    jint status = jvm_->AttachCurrentThread((void **) &env, nullptr);
+    jint status = jvm_->AttachCurrentThread((void**) &env, nullptr);
     assert(status == JNI_OK);
     string registry_class_name_;
     OP_REQUIRES_OK(ctx, ctx->GetAttr("registry_class_name", &registry_class_name_));
     registry_ = env->FindClass(registry_class_name_.c_str());
     call_method_id_ = env->GetStaticMethodID(registry_, "call", "(I[J)[J");
+    status = jvm_->DetachCurrentThread();
+    assert(status == JNI_OK);
     if (call_method_id_ == nullptr) {
       ctx->CtxFailure(errors::InvalidArgument("Missing JVM registry 'call' method."));
       return;
@@ -165,7 +166,6 @@ class JVMCallbackOp : public OpKernel {
     Status s = CallJVMFunction(&call);
     status = jvm_->DetachCurrentThread();
     assert(status == JNI_OK);
-    if (env->ExceptionCheck()) return;
 
     OP_REQUIRES_OK(ctx, s);
 
@@ -184,7 +184,7 @@ class JVMCallbackOp : public OpKernel {
     }
   }
 
- private:
+private:
   int id_;
   JavaVM* jvm_;
   jclass registry_;
@@ -195,4 +195,5 @@ class JVMCallbackOp : public OpKernel {
 
 REGISTER_KERNEL_BUILDER(Name("JVMCallback").Device(DEVICE_CPU), JVMCallbackOp);
 REGISTER_KERNEL_BUILDER(Name("JVMCallbackStateless").Device(DEVICE_CPU), JVMCallbackOp);
+
 }  // namespace tensorflow
