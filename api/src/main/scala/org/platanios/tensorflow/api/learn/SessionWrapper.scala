f7156267b602c93b58a62c89a014c14e73a6626a
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-09-20 12:53:01 -0400

    Added a couple more session wrappers.

--- api/src/main/scala/org/platanios/tensorflow/api/learn/SessionWrapper.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/learn/SessionWrapper.scala
@@ -15,9 +15,15 @@
 
 package org.platanios.tensorflow.api.learn
 
-import org.platanios.tensorflow.api.core.client.Session
+import org.platanios.tensorflow.api.core.client.{Executable, FeedMap, Fetchable, Session}
+import org.platanios.tensorflow.api.ops.{Op, Output}
 
-import scala.util.control
+import com.typesafe.scalalogging.Logger
+import org.slf4j.LoggerFactory
+import org.tensorflow.framework.{RunMetadata, RunOptions}
+
+import scala.util.{Failure, Success, Try}
+import scala.util.control.Exception._
 
 /** Wrapper around a [[Session]].
   *
@@ -31,7 +37,7 @@ import scala.util.control
   *
   * @author Emmanouil Antonios Platanios
   */
-abstract class SessionWrapper(session: Session)
+abstract class SessionWrapper private[learn](protected var session: Session)
     extends Session(session.graphReference, session.nativeHandle, session.target) {
   private[this] var _closed: Boolean = false
 
@@ -55,7 +61,7 @@ abstract class SessionWrapper(session: Session)
 
   override def close(): Unit = {
     if (!closed) {
-      control.Exception.ignoring(SessionWrapper.PREEMPTION_ERRORS.toSeq: _*)(super.close())
+      ignoring(SessionWrapper.PREEMPTION_ERRORS.toSeq: _*)(super.close())
       _closed = true
     }
   }
@@ -63,5 +69,144 @@ abstract class SessionWrapper(session: Session)
 
 object SessionWrapper {
   // TODO: !!! We need to fill this and find a way to more generally better represent TensorFlow exceptions.
-  private[SessionWrapper] val PREEMPTION_ERRORS: Set[Class[_]] = Set(classOf[IndexOutOfBoundsException])
+  private[learn] val PREEMPTION_ERRORS: Set[Class[_]] = Set.empty // Set(classOf[IndexOutOfBoundsException])
+}
+
+/** Session wrapper that recreates a session upon certain kinds of errors.
+  *
+  * The constructor is passed a [[SessionCreator]] object, not a [[Session]].
+  *
+  * TODO: !!! Change the names of acceptable exceptions.
+  * Calls to `run()` are delegated to the wrapped session. If a call throws an `AbortedError` or an `UnavailableError`,
+  * the wrapped session is closed, and a new one is created by invoking the session creator.
+  *
+  * @param  sessionCreator Factory for creating new sessions.
+  *
+  * @author Emmanouil Antonios Platanios
+  */
+class RecoverableSession private[learn](sessionCreator: SessionCreator)
+    extends SessionWrapper(RecoverableSession.createSession(sessionCreator)) {
+  override protected def checkStop: Boolean = {
+    if (closed) {
+      // If the session has been closed, computation should stop.
+      true
+    } else {
+      // If any exception is thrown, we should stop.
+      Try(catching(SessionWrapper.PREEMPTION_ERRORS.toSeq: _*).withApply(e => {
+        RecoverableSession.logger.info(
+          "An exception was thrown while considering whether the session is complete. This may be due to a " +
+              "preemption in a connected worker or parameter server. The current session will be closed and a new " +
+              "session will be created. Exception: " + e)
+        this.close()
+        session = sessionCreator.createSession()
+        // Since we have just recreated the session, the overall computation should not stop.
+        false
+      }).apply({
+        session match {
+          case s: SessionWrapper => s.checkStop
+          case _ => false
+        }
+      })).getOrElse(true)
+    }
+  }
+
+  override protected def runHelper[F, E, R](
+      feeds: FeedMap = FeedMap.empty, fetches: F = Seq.empty[Output], targets: E = Traversable.empty[Op],
+      options: RunOptions = null, wantMetadata: Boolean = false
+  )(implicit
+      executable: Executable[E],
+      fetchable: Fetchable.Aux[F, R]
+  ): (R, Option[RunMetadata]) = {
+    var result: (R, Option[RunMetadata]) = null
+    while (result == null) {
+      if (closed)
+        session = RecoverableSession.createSession(sessionCreator)
+      handling(SessionWrapper.PREEMPTION_ERRORS.toSeq: _*).by(e => {
+        RecoverableSession.logger.info(
+          "An exception was thrown. This may be due to a preemption in a connected worker or parameter server. " +
+              "The current session will be closed and a new session will be created. Exception: " + e)
+        close()
+      }) {
+        result = session.runHelper(feeds, fetches, targets, options, wantMetadata)
+      }
+    }
+    result
+  }
+}
+
+object RecoverableSession {
+  private[RecoverableSession] val logger = Logger(LoggerFactory.getLogger("Learn / Recoverable Session"))
+
+  private[RecoverableSession] def createSession(sessionCreator: SessionCreator): Session = {
+    var session: Session = null
+    while (session == null) {
+      handling(SessionWrapper.PREEMPTION_ERRORS.toSeq: _*).by(e => RecoverableSession.logger.info(
+        "An exception was thrown while a session was being created. This may be due to a preemption of a connected " +
+            "worker or parameter server. A new session will be created. Exception: " + e)) {
+        session = sessionCreator.createSession()
+      }
+    }
+    session
+  }
+}
+
+/** Session wrapper that works with a [[Coordinator]].
+  *
+  * Calls to `run()` are delegated to the wrapped session. If a call throws an exception, the exception is reported to
+  * the coordinator.
+  *
+  * In addition, after each call to `run()` this session asks the coordinator if the session should stop. In that case,
+  * it will will join all the threads registered with the coordinator before returning.
+  *
+  * If the coordinator was requested to stop with an exception, that exception will be re-thrown from the call to
+  * `run()`.
+  *
+  * @param  session                Session being wrapped.
+  * @param  coordinator            Coordinator to use.
+  * @param  stopGracePeriodSeconds Grace period (i.e., number of seconds) given to threads to stop after `close()` has
+  *                                been called.
+  *
+  * @author Emmanouil Antonios Platanios
+  */
+class CoordinatedSession private[learn](session: Session, coordinator: Coordinator, stopGracePeriodSeconds: Int = 120)
+  extends SessionWrapper(session) {
+  override protected def checkStop: Boolean = {
+    // If the coordinator was asked to stop due to an exception, then that exception needs to be propagated.
+    coordinator.reThrowRequestedStopCause()
+    // At this point, no exceptions are recorded in the coordinator.
+    coordinator.shouldStop
+  }
+
+  override def close(): Unit = {
+    coordinator.requestStop()
+    try {
+      coordinator.join(gracePeriodSeconds = stopGracePeriodSeconds, ignoreLiveThreads = true)
+    } finally {
+      // We intentionally suppress exceptions from the `close()` here since any useful exceptions have are already been
+      // reported by `join()`.
+      Try(super.close())
+    }
+  }
+
+  override protected def runHelper[F, E, R](
+      feeds: FeedMap = FeedMap.empty, fetches: F = Seq.empty[Output], targets: E = Traversable.empty[Op],
+      options: RunOptions = null, wantMetadata: Boolean = false
+  )(implicit
+      executable: Executable[E],
+      fetchable: Fetchable.Aux[F, R]
+  ): (R, Option[RunMetadata]) = {
+    Try(session.runHelper(feeds, fetches, targets, options, wantMetadata)) match {
+      case Success(result) => result
+      case Failure(cause) =>
+        // A non-preemption exception could have been caused by a preemption error in the coordinator. If this is the
+        // case, raise that exception instead because it's the root cause. Otherwise, stick to the original cause.
+        try {
+          coordinator.reThrowRequestedStopCause()
+          throw cause
+        } catch {
+          case e if SessionWrapper.PREEMPTION_ERRORS.contains(e.getClass) => throw e
+          case _ => throw cause
+        }
+    }
+  }
 }
