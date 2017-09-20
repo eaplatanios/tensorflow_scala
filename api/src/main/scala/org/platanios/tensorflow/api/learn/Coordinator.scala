0355987301d5720e39511dcac89bd63e6f0c42d3
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-09-20 12:51:28 -0400

    Made some improvements and fixes to the coordinator class.

--- api/src/main/scala/org/platanios/tensorflow/api/learn/Coordinator.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/learn/Coordinator.scala
@@ -15,18 +15,103 @@
 
 package org.platanios.tensorflow.api.learn
 
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.locks.{Condition, Lock, ReentrantLock}
-
 import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.locks.{Condition, Lock, ReentrantLock}
+
 import scala.collection.mutable
 
-/**
+/** Coordinator for threads.
+  *
+  * This class implements a simple mechanism to coordinate the termination of a set of threads.
+  *
+  * === Usage ===
+  *
+  * {{{
+  *   // Create a coordinator.
+  *   val coordinator = Coordinator()
+  *   // Start a number of threads, optionally passing a coordinator to each of them.
+  *   ...start thread 1... (coordinator, ...)
+  *   ...
+  *   ...start thread N... (coordinator, ...)
+  *   // Wait for all the threads to terminate.
+  *   coordinator.join(threads)
+  * }}}
+  *
+  * Any of the threads can call `coordinator.requestStop()` to ask for all the threads to stop. To cooperate with the
+  * requests, each thread must check for the `coordinator.shouldStop` flag on a regular basis. `coordinator.shouldStop`
+  * returns `true` as soon as `coordinator.requestStop()` has been called.
+  *
+  * A typical thread running with a coordinator will do something like:
+  * {{{
+  *   while (!coordinator.shouldStop) {
+  *     ...do some work...
+  *   }
+  * }}}
+  *
+  * === Exception Handling ===
+  *
+  * A thread can report an exception to the coordinator as part of the `requestStop()` call. The exception will be
+  * re-thrown from the `coordinator.join()` call.
+  *
+  * Example thread code:
+  * {{{
+  *   try {
+  *     ...do some work...
+  *   } catch {
+  *     case e: Throwable => coordinator.requestStop(e)
+  *   }
+  * }}}
+  *
+  * Main process example code:
+  * {{{
+  *   try {
+  *     ...
+  *     val coordinator = Coordinator()
+  *     // Start a number of threads, optionally passing a coordinator to each of them.
+  *     ...start thread 1... (coordinator, ...)
+  *     ...
+  *     ...start thread N... (coordinator, ...)
+  *     // Wait for all the threads to terminate.
+  *     coordinator.join(threads)
+  *   } catch {
+  *     case e: Throwable => ...handle the exception that was passed to `requestStop()`...
+  *   }
+  * }}}
+  *
+  * === Grace Period for Stopping ===
+  *
+  * After a thread has called `coordinator.requestStop()` the other threads have a fixed and limited amount of time to
+  * stop, this is called the *grace period* and defaults to 120 seconds. If any of the threads is still alive after the
+  * grace period expires, `coordinator.join()` raises a [[RuntimeException]] reporting the laggards.
+  *
+  * Example:
+  * {{{
+  *   try {
+  *     ...
+  *     val coordinator = Coordinator()
+  *     // Start a number of threads, optionally passing a coordinator to each of them.
+  *     ...start thread 1... (coordinator, ...)
+  *     ...
+  *     ...start thread N... (coordinator, ...)
+  *     // Wait for all the threads to terminate.
+  *     coordinator.join(threads, gracePeriodSeconds = 10)
+  *   } catch {
+  *     case e: RuntimeException =>
+  *     case e: Throwable => ...handle the exception that was passed to `requestStop()`...
+  *   }
+  * }}}
+  *
+  * @param  cleanStopExceptionTypes Exception types that should cause a clean stop of the coordinator. If an exception
+  *                                 of one of these types is reported to `requestStop()` the coordinator will behave as
+  *                                 if no exception was reported. Defaults to `Set(classOf[IndexOutOfBoundsException])`
+  *                                 which is used by input queues to signal the end of input.
+  *
   * @author Emmanouil Antonios Platanios
   */
-case class Coordinator private[training]() {
+case class Coordinator(cleanStopExceptionTypes: Set[Class[_]] = Set(classOf[IndexOutOfBoundsException])) {
   private[this] val logger   : Logger            = Logger(LoggerFactory.getLogger("Coordinator"))
   private[this] val lock     : Lock              = new ReentrantLock
   private[this] val stopEvent: Coordinator.Event = Coordinator.Event()
@@ -88,16 +173,17 @@ case class Coordinator private[training]() {
     *               then that cause is recorded and is thrown from [[join]].
     */
   def requestStop(cause: Option[Throwable] = None): Unit = {
+    val filteredCause = cause.filter(t => !cleanStopExceptionTypes.contains(t.getClass))
     lock.lock()
     try {
       // If we have already joined the coordinator the exception will not have a chance to be reported, so we just raise
       // it normally. This can happen if one continues to use a session after having joined the coordinator threads.
       if (joined)
-        cause.foreach(throw _)
+        filteredCause.foreach(throw _)
       if (!stopEvent.isSet) {
-        if (cause.isDefined && causeForStop.isEmpty) {
-          logger.info("Error reported to the coordinator.", cause)
-          causeForStop = cause
+        if (filteredCause.isDefined && causeForStop.isEmpty) {
+          logger.info("Exception reported to the coordinator.", filteredCause)
+          causeForStop = filteredCause
         }
         stopEvent.set()
       }
@@ -128,16 +214,16 @@ case class Coordinator private[training]() {
     * alive after that period expires and `ignoreLiveThreads` is `false`, a [[RuntimeException]] is thrown. Now that is
     * a `cause` was passed to [[requestStop]] then that cause is thrown instead of that [[RuntimeException]].
     *
-    * @param  threads           Set of threads to join along with the registered threads.
-    * @param  gracePeriod       Number of seconds given to threads to stop after [[requestStop]] has been called.
-    * @param  ignoreLiveThreads If `false`, a [[RuntimeException]] is thrown is any of the threads are still alive after
-    *                           the grace period expires. If `true`, the thread names are logged, but no exception is
-    *                           thrown.
-    * @throws RuntimeException If any of the threads are still alive after the grace period expires and
-    *                          `ignoreLiveThreads` is set to `false`.
+    * @param  threads            Set of threads to join along with the registered threads.
+    * @param  gracePeriodSeconds Number of seconds given to threads to stop after [[requestStop]] has been called.
+    * @param  ignoreLiveThreads  If `false`, a [[RuntimeException]] is thrown is any of the threads are still alive
+    *                            after the grace period expires. If `true`, the thread names are logged, but no
+    *                            exception is thrown.
+    * @throws RuntimeException   If any of the threads are still alive after the grace period expires and
+    *                            `ignoreLiveThreads` is set to `false`.
     */
   @throws[RuntimeException]
-  def join(threads: Set[Thread] = Set.empty, gracePeriod: Int = 120, ignoreLiveThreads: Boolean = false): Unit = {
+  def join(threads: Set[Thread] = Set.empty, gracePeriodSeconds: Int = 120, ignoreLiveThreads: Boolean = false): Unit = {
     // Threads registered after this call will not be joined.
     val allThreads = {
       lock.lock()
@@ -154,7 +240,7 @@ case class Coordinator private[training]() {
     // If any thread is still alive, wait for the grace period to expire. By the time this check is executed, threads
     // may still be shutting down, so we add a sleep of increasing duration to give them a chance to shut down without
     // losing too many cycles. Also, the sleep duration is limited to the remaining grace period duration.
-    var remainingGracePeriod = gracePeriod / 1000
+    var remainingGracePeriod = gracePeriodSeconds / 1000
     var sleepTime = 1
     while (allThreads.exists(_.isAlive) && remainingGracePeriod >= 0.0) {
       Thread.sleep(sleepTime)
@@ -184,13 +270,19 @@ case class Coordinator private[training]() {
       lock.unlock()
     }
   }
+
+  /** If an exception has been passed to `requestStop()`, this method re-throws it. */
+  def reThrowRequestedStopCause(): Unit = {
+    lock.lock()
+    try {
+      causeForStop.foreach(throw _)
+    } finally {
+      lock.unlock()
+    }
+  }
 }
 
-private[training] object Coordinator {
-  private[training] trait API {
-    def coordinator(): Coordinator = Coordinator()
-  }
-
+private[learn] object Coordinator {
   private[Coordinator] case class Event() {
     private[this] val lock: Lock      = new ReentrantLock
     private[this] val cond: Condition = lock.newCondition
