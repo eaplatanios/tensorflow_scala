95b6b80166e0240957155cae2225eb0e2e7dc5ef
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-09-21 23:32:38 -0400

    Minor bug fix (reversion of previous commit and fix).

--- api/src/main/scala/org/platanios/tensorflow/api/learn/hooks/HookTrigger.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/learn/hooks/HookTrigger.scala
@@ -20,6 +20,9 @@ package org.platanios.tensorflow.api.learn.hooks
   * @author Emmanouil Antonios Platanios
   */
 trait HookTrigger {
+  /** Returns a copy of this hook trigger that is also reset. */
+  def copy(): HookTrigger
+
   /** Resets the internal state of this trigger (e.g., step counter or timer). */
   def reset(): Unit
 
@@ -41,6 +44,9 @@ trait HookTrigger {
 
 /** Hook trigger that never actually triggers. */
 case object NoHookTrigger extends HookTrigger {
+  /** Returns a copy of this hook trigger that is also reset. */
+  override def copy(): HookTrigger = this
+
   /** Resets the internal state of this trigger (e.g., step counter or timer). */
   override def reset(): Unit = ()
 
@@ -66,6 +72,9 @@ case object NoHookTrigger extends HookTrigger {
   */
 case class StepHookTrigger(numSteps: Int) extends HookTrigger {
   require(numSteps >= 0, s"'numSteps' (= $numSteps) must be a non-negative number.")
+
+  /** Returns a copy of this hook trigger that is also reset. */
+  override def copy(): HookTrigger = super.copy()
 
   private[this] var _lastTrigger: Option[(Float, Int)] = None
 
@@ -103,6 +112,9 @@ case class StepHookTrigger(numSteps: Int) extends HookTrigger {
   */
 case class TimeHookTrigger(numSeconds: Float) extends HookTrigger {
   require(numSeconds >= 0, s"'numSeconds' (= $numSeconds) must be a non-negative number.")
+
+  /** Returns a copy of this hook trigger that is also reset. */
+  override def copy(): HookTrigger = super.copy()
 
   private[this] var _lastTrigger: Option[(Float, Int)] = None
 
