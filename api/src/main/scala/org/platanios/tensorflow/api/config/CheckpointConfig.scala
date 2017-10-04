cdde63994a57bf8acf2527812bbc6fd950c8a25e
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-10-04 02:52:55 -0400

    Minor edit to remove a compiler warning.

--- api/src/main/scala/org/platanios/tensorflow/api/config/CheckpointConfig.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/config/CheckpointConfig.scala
@@ -30,26 +30,6 @@ case object NoCheckpoints extends CheckpointConfig {
   override val keepCheckpointEveryNHours: Int = 10000
 }
 
-/** Checkpoint configuration base class.
-  *
-  * @param  maxCheckpointsToKeep      Maximum number of recent checkpoint files to keep. As new files are created, older
-  *                                   files are deleted. If 0, then all checkpoint files are kept. Defaults to 5 (that
-  *                                   is, the 5 most recent checkpoint files are kept).
-  * @param  keepCheckpointEveryNHours Number of hours between each checkpoint to be saved. The default value of 10,000
-  *                                   hours effectively disables the feature.
-  */
-private[config] abstract class CheckpointConfigBase private[config](
-    override val maxCheckpointsToKeep: Int = 5,
-    override val keepCheckpointEveryNHours: Int = 10000
-) extends CheckpointConfig {
-  require(
-    maxCheckpointsToKeep >= 0,
-    s"'maxCheckpointsToKeep' (set to $maxCheckpointsToKeep) needs to be a non-negative integer.")
-  require(
-    keepCheckpointEveryNHours > 0,
-    s"'checkpointEveryNHours' (set to $keepCheckpointEveryNHours) needs to be a positive integer.")
-}
-
 /** Checkpoint configuration for step-based checkpoints (i.e., checkpoints every `n` steps).
   *
   * @param  steps                     Save checkpoints every this many steps.
@@ -61,10 +41,16 @@ private[config] abstract class CheckpointConfigBase private[config](
   */
 case class StepBasedCheckpoints(
     steps: Int = 1000,
-    override val maxCheckpointsToKeep: Int = 5,
-    override val keepCheckpointEveryNHours: Int = 10000
-) extends CheckpointConfigBase(maxCheckpointsToKeep, keepCheckpointEveryNHours) {
+    maxCheckpointsToKeep: Int = 5,
+    keepCheckpointEveryNHours: Int = 10000
+) extends CheckpointConfig {
   require(steps >= 0, s"'steps' (set to $steps) needs to be a non-negative integer.")
+  require(
+    maxCheckpointsToKeep >= 0,
+    s"'maxCheckpointsToKeep' (set to $maxCheckpointsToKeep) needs to be a non-negative integer.")
+  require(
+    keepCheckpointEveryNHours > 0,
+    s"'checkpointEveryNHours' (set to $keepCheckpointEveryNHours) needs to be a positive integer.")
 }
 
 /** Checkpoint configuration for time-based checkpoints (i.e., checkpoints every `n` seconds).
@@ -80,6 +66,12 @@ case class TimeBasedCheckpoints(
     seconds: Int = 600,
     override val maxCheckpointsToKeep: Int = 5,
     override val keepCheckpointEveryNHours: Int = 10000
-) extends CheckpointConfigBase(maxCheckpointsToKeep, keepCheckpointEveryNHours) {
+) extends CheckpointConfig {
   require(seconds >= 0, s"'seconds' (set to $seconds) needs to be a non-negative integer.")
+  require(
+    maxCheckpointsToKeep >= 0,
+    s"'maxCheckpointsToKeep' (set to $maxCheckpointsToKeep) needs to be a non-negative integer.")
+  require(
+    keepCheckpointEveryNHours > 0,
+    s"'checkpointEveryNHours' (set to $keepCheckpointEveryNHours) needs to be a positive integer.")
 }
