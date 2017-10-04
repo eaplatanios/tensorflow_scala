d0aea4ba73801de4610e29a99b79d79ee85defee
Anthony Platanios <anthonyplat719791@hotmail.com>
2017-10-04 14:05:13 -0400

    Minor bug fix.

--- api/src/main/scala/org/platanios/tensorflow/api/ops/control_flow/ControlFlow.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/ops/control_flow/ControlFlow.scala
@@ -204,7 +204,7 @@ private[api] trait ControlFlow {
     *         return structure of `bodyFn`.
     */
   def whileLoop[T, TS](
-      predicateFn: T => Output, bodyFn: T => T, loopVariables: T, shapeInvariants: TS = null,
+      predicateFn: T => Output, bodyFn: T => T, loopVariables: T, shapeInvariants: TS = null.asInstanceOf[TS],
       parallelIterations: Int = 10, enableBackPropagation: Boolean = true, swapMemory: Boolean = false,
       name: String = "WhileLoop")(implicit
       ev: WhileLoopVariable.Aux[T, TS]
