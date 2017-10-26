51d0dd9500012f4e307ee898d4685c43e976638b
Anthony Platanios <e.a.platanios@gmail.com>
2017-10-26 15:18:34 -0400

    Bug fix in the directory loader.

--- api/src/main/scala/org/platanios/tensorflow/api/io/DirectoryLoader.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/io/DirectoryLoader.scala
@@ -33,16 +33,17 @@ import java.nio.file.Path
   * and never come back. It uses some heuristics to check whether this is true based on tracking changes to the files'
   * sizes, but the checks can yield false negatives. However, they should never yield false positives.
   *
-  * @param  path          Directory to watch.
+  * @param  directory     Directory to watch.
   * @param  loaderFactory Factory for loaders.
   * @param  pathFilter    Optional path filter to use.
   *
   * @author Emmanouil Antonios Platanios
   */
 case class DirectoryLoader[T](
-    private var path: Path,
+    private val directory: Path,
     private val loaderFactory: (Path) => Loader[T],
     private val pathFilter: (Path) => Boolean = _ => true) {
+  private[this] var _path: Path = _
   private[this] var _loader                  : Loader[T] = _
   private[this] var _outOfOrderWritesDetected: Boolean         = false
   private[this] var _finalizedSizes          : Map[Path, Long] = Map.empty[Path, Long]
@@ -72,9 +73,9 @@ case class DirectoryLoader[T](
   def load(): Iterator[T] = new Iterator[T] {
     private[this] var currentIterator: Iterator[T] = {
       if (_loader == null) {
-        path = nextPath()
-        if (path != null)
-          setPath(path)
+        _path = nextPath()
+        if (_path != null)
+          setPath(_path)
       }
       if (_loader == null)
         null
@@ -88,30 +89,34 @@ case class DirectoryLoader[T](
       if (!currentIterator.hasNext) {
         val next = nextPath()
         if (next == null) {
-          DirectoryLoader.logger.info(s"No path found after '$path'.")
+          DirectoryLoader.logger.info(s"No path found after '${_path}'.")
           // The current path is empty and there are no new paths, and so we are done.
           currentIterator = null
-        } else if (!secondPass) {
-          // There is a new path and so we check to make sure there were not any entries written between when we
-          // finished reading the current path and when we checked for the new one. The sequence of entries might look
-          // something like this:
-          //   1. Entry #1 written to path #1.
-          //   2. We check for entries and yield entry #1 from path #1.
-          //   3. We check for entries and see that there are no more entries in path #1.
-          //   4. Entry #2 is written to path #1.
-          //   5. Entry #3 is written to path #2.
-          //   6. We check for a new path and see that path #2 exists.
-          // Without this loop, we would miss entry #2. We are also guaranteed by the reader contract that no more
-          // entries will be written to path #1 after entries start being written to path #2, and so we do not have to
-          // worry about that.
-          currentIterator = _loader.load()
-          secondPass = true
         } else {
-          DirectoryLoader.logger.info(s"Directory loader advancing from '$path' to '$next'.")
-          secondPass = false
-          // Advance to the next path and start over.
-          setPath(next)
-          currentIterator = _loader.load()
+          if (!secondPass) {
+            // There is a new path and so we check to make sure there were not any entries written between when we
+            // finished reading the current path and when we checked for the new one. The sequence of entries might look
+            // something like this:
+            //   1. Entry #1 written to path #1.
+            //   2. We check for entries and yield entry #1 from path #1.
+            //   3. We check for entries and see that there are no more entries in path #1.
+            //   4. Entry #2 is written to path #1.
+            //   5. Entry #3 is written to path #2.
+            //   6. We check for a new path and see that path #2 exists.
+            // Without this loop, we would miss entry #2. We are also guaranteed by the reader contract that no more
+            // entries will be written to path #1 after entries start being written to path #2, and so we do not have to
+            // worry about that.
+            currentIterator = _loader.load()
+          }
+          if (currentIterator.hasNext) {
+            secondPass = true
+          } else {
+            DirectoryLoader.logger.info(s"Directory loader advancing from '${_path}' to '$next'.")
+            secondPass = false
+            // Advance to the next path and start over.
+            setPath(next)
+            currentIterator = _loader.load()
+          }
         }
       }
     }
@@ -128,8 +133,8 @@ case class DirectoryLoader[T](
         }
       } catch {
         case _: Throwable =>
-          if (!FileIO.fileExists(path))
-            throw DirectoryDeletedException(s"Directory '$path' has been permanently deleted.")
+          if (!FileIO.fileExists(directory))
+            throw DirectoryDeletedException(s"Directory '$directory' has been permanently deleted.")
           false
       }
     }
@@ -140,8 +145,8 @@ case class DirectoryLoader[T](
         maybeNextPath()
       } catch {
         case _: Throwable =>
-          if (!FileIO.fileExists(path))
-            throw DirectoryDeletedException(s"Directory '$path' has been permanently deleted.")
+          if (!FileIO.fileExists(directory))
+            throw DirectoryDeletedException(s"Directory '$directory' has been permanently deleted.")
       }
       event
     }
@@ -150,13 +155,13 @@ case class DirectoryLoader[T](
   /** Gets the next path to load entries from. This method also does the checking for out-of-order writes as it iterates
     * through the paths. */
   private[this] def nextPath(): Path = {
-    val sortedPaths = FileIO.listDirectories(path).map(path.resolve(_)).filter(pathFilter).sortBy(_.toString)
+    val sortedPaths = FileIO.listDirectories(directory).map(directory.resolve).filter(pathFilter).sortBy(_.toString)
     if (sortedPaths.isEmpty) {
       null
-    } else if (path == null) {
+    } else if (_path == null) {
       sortedPaths.head
     } else {
-      val currentPathIndex = sortedPaths.indexOf(path)
+      val currentPathIndex = sortedPaths.indexOf(_path)
       // Do not bother checking if the paths are in GCS (which we cannot check) or if we have already detected an
       // out-of-order write.
       if (!FileIO.isGCSPath(sortedPaths.head) && !outOfOrderWritesDetected) {
@@ -164,14 +169,14 @@ case class DirectoryLoader[T](
         val outOfOrderCheckStart = math.max(0, currentPathIndex - OUT_OF_ORDER_WRITE_CHECK_COUNT)
         _outOfOrderWritesDetected = sortedPaths.slice(outOfOrderCheckStart, currentPathIndex).exists(hasOutOfOrderWrite)
       }
-      sortedPaths.drop(currentPathIndex).headOption.orNull
+      sortedPaths.drop(currentPathIndex + 1).headOption.orNull
     }
   }
 
   /** Sets the current path to watch for new entries to `path`. This method also records the size of the old path, if
     * any. If the size cannot be determined, an error is logged. */
   private[this] def setPath(path: Path): Unit = {
-    val oldPath = this.path
+    val oldPath = this._path
     if (oldPath != null && !FileIO.isGCSPath(oldPath)) {
       try {
         // We are done with the path, and so we store its size.
@@ -182,7 +187,7 @@ case class DirectoryLoader[T](
         case t: Throwable => DirectoryLoader.logger.error(s"Unable to get the size of '$oldPath'.", t)
       }
     }
-    this.path = path
+    this._path = path
     this._loader = loaderFactory(path)
   }
 
@@ -194,10 +199,10 @@ case class DirectoryLoader[T](
     if (size != oldSize) {
       if (oldSize == -1L)
         DirectoryLoader.logger.error(
-          s"File '$path' created after file '${this.path}' " +
+          s"File '$path' created after file '${this._path}' " +
               s"even though its name lexicographical order indicates otherwise.")
       else
-        DirectoryLoader.logger.error(s"File '$path' updated even though the current file is '${this.path}'.")
+        DirectoryLoader.logger.error(s"File '$path' updated even though the current file is '${this._path}'.")
       true
     } else {
       false
