baf4c3ebe97dc89690c4cb316f8eef86f796e831
Anthony Platanios <e.a.platanios@gmail.com>
2018-03-12 20:27:56 -0400

    [OPS] Minor bug fix.

--- api/src/main/scala/org/platanios/tensorflow/api/ops/training/optimizers/AMSGrad.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/ops/training/optimizers/AMSGrad.scala
@@ -15,6 +15,7 @@
 
 package org.platanios.tensorflow.api.ops.training.optimizers
 
+import org.platanios.tensorflow.api.implicits.Implicits._
 import org.platanios.tensorflow.api.ops.{Basic, Math, Op, Output, OutputIndexedSlices, Summary}
 import org.platanios.tensorflow.api.ops.control_flow.ControlFlow
 import org.platanios.tensorflow.api.ops.training.optimizers.schedules.{FixedSchedule, Schedule}
