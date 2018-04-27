4f0f8e409f30ccfba688f3ce1a231ee0e685f8d4
Anthony Platanios <e.a.platanios@gmail.com>
2018-04-27 14:14:01 -0400

    [OPS] Improvement in documentation.

--- api/src/main/scala/org/platanios/tensorflow/api/ops/training/optimizers/LazyAMSGrad.scala
+++ api/src/main/scala/org/platanios/tensorflow/api/ops/training/optimizers/LazyAMSGrad.scala
@@ -8,6 +8,13 @@ import org.platanios.tensorflow.api.ops.variables.Variable
 /** Optimizer that implements a variant of the AMSGrad optimization algorithm that handles sparse updates more
   * efficiently.
   *
+  * The original AMSGrad algorithm maintains three moving-average accumulators for each trainable variable; the
+  * accumulators are updated at every step. This class provides lazier handling of gradient updates for sparse
+  * variables. It only updates the moving-average accumulators for sparse variable indices that appear in the current
+  * batch, rather than updating the accumulators for all indices. Compared with the original Adam optimizer, it can
+  * provide large improvements in model training throughput for some applications. However, it provides slightly
+  * different semantics than the original Adam algorithm, and may lead to different empirical results.
+  *
   * Initialization:
   * {{{
   *   m_0 = 0     // Initialize the 1st moment vector
