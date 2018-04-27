b5ff2acbedc41fc3225baeb52dda2ca941d5f6e5
lucatagliabue <luca.tagliabue@radicalbit.io>
2018-04-27 18:41:32 +0200

    refactor code style

--- examples/src/main/scala/org/platanios/tensorflow/examples/python2scala/LinearRegressionFromRestoredPythonModel.scala
+++ examples/src/main/scala/org/platanios/tensorflow/examples/python2scala/LinearRegressionFromRestoredPythonModel.scala
@@ -6,7 +6,7 @@ import java.nio.file.Paths
 import com.typesafe.scalalogging.Logger
 import org.platanios.tensorflow.api.core.Graph
 import org.platanios.tensorflow.api.ops.Output
-import org.platanios.tensorflow.api.{Session, Shape, Tensor, tf}
+import org.platanios.tensorflow.api.{Op, Session, Shape, Tensor, tf}
 import org.slf4j.LoggerFactory
 import org.tensorflow.framework.MetaGraphDef
 
@@ -32,9 +32,7 @@ object LinearRegressionFromRestoredPythonModel {
     val meta = "examples/src/main/scala/org/platanios/tensorflow/examples/python2scala/virgin-linear-regression-pull-request.meta"
     val checkpoint = "examples/src/main/scala/org/platanios/tensorflow/examples/python2scala/virgin-linear-regression-pull-request"
     val metaGraphDefFile = "examples/src/main/scala/org/platanios/tensorflow/examples/python2scala/MetaGraphDef.txt"
-    
-    logger.info("\n- - - - - PYTHON 2 SCALA - - - - - ")
-    // Path to a model saved with the Python API of tensorflow (version 1.7.0)
+
     val metaGraphDefInputStream = new BufferedInputStream(new FileInputStream(meta))
     val mgf = MetaGraphDef.parseFrom(metaGraphDefInputStream)
     val checkpointPath = Paths.get(checkpoint)
@@ -47,14 +45,11 @@ object LinearRegressionFromRestoredPythonModel {
 
       // ----- PRINT OPERATIONS IN THE GRAPH ---------
       println(" -" * 40)
-      for (op <- session.graph.ops) {
+      for (op <- session.graph.ops)
         myPrintln("OPERATION name:    " + op, 76)
-      }
       println(" -" * 40)
-      // ---------------------------------------------
 
-      //tf.Tensor in Python is the equivalent of Output here in scala
-      // NODES
+      // RESTORE NODES and OPERATIONS
       val input = session.graph.getOutputByName("p2s_input:0")
       val output = session.graph.getOutputByName("p2s_output:0")
       val weight = session.graph.getOutputByName("p2s_weights_w/Read/ReadVariableOp:0")
@@ -65,43 +60,22 @@ object LinearRegressionFromRestoredPythonModel {
       val gradientW = session.graph.getOutputByName("exported_gradient_p2s_weights_w:0")
       val gradientB = session.graph.getOutputByName("exported_gradient_p2s_weights_b:0")
 
-      // OPERATIONS
       val trainOp = session.graph.getOpByName("p2s_train_op")
-      val assign_variable_op_w = session.graph.getOpByName("AssignVariableOp")
-      val assign_variable_op_b = session.graph.getOpByName("AssignVariableOp_1")
+      val assignVariableW = session.graph.getOpByName("AssignVariableOp")
+      val assignVariableB = session.graph.getOpByName("AssignVariableOp_1")
 
-      // ---------- PRINT RESTORED OUTPUT AND OPERATIONS ------------
-      println(" *" * 60)
-      myPrintln("Trained weight value: " + session.run(fetches = weight).scalar, 90)
-      myPrintln("Trained bias value: " + session.run(fetches = bias).scalar, 90)
-      myPrintln("" + input, 116)
-      myPrintln("" + output, 116)
-      myPrintln("" + weight, 116)
-      myPrintln("" + bias, 116)
-      myPrintln("" + loss, 116)
-      myPrintln("" + placeholderW, 116)
-      myPrintln("" + placeholderB, 116)
-      myPrintln("" + gradientW, 116)
-      myPrintln("" + gradientB, 116)
+      printRestoredNodesAndOperations(session, input, output, weight, bias, loss, placeholderW, placeholderB, gradientW, gradientB, trainOp, assignVariableW, assignVariableB)
 
-      myPrintln("" + trainOp, 116)
-      myPrintln("" + assign_variable_op_w, 116)
-      myPrintln("" + assign_variable_op_b, 116)
-      println(" *" * 60)
-      // ---------------------------------------------
-
-      println("")
-
-      // TRAINING
+      // TRAINING LOOP
       for (i <- 0 to 50) {
         val trainBatch: (Tensor, Tensor) = batch(10000)
         val feedsMap: Map[Output, Tensor] = Map(input -> trainBatch._1, output -> trainBatch._2)
-        val fetchesSeq = Seq(loss, weight)
+        val fetchesSeq = Seq(loss, weight, bias)
         val trainFetches = session.run(feeds = feedsMap, fetches = fetchesSeq, targets = trainOp)
-        val trainLoss = trainFetches.head
+        val trainLoss = trainFetches(0)
         val trainWeight = trainFetches(1)
-        logger.info(s"Train loss at iteration $i = ${trainLoss.scalar} ")
-        logger.info(s"Train weight at iteration $i = ${trainWeight.scalar}\n")
+        val trainBias = trainFetches(2)
+        logger.info(s"Train loss at iteration $i = ${trainLoss.scalar}\nTrain weight at iteration $i = ${trainWeight.scalar}\nTrain bias at iteration $i = ${trainBias.scalar}")
       }
     }
   }
@@ -119,6 +93,29 @@ object LinearRegressionFromRestoredPythonModel {
     (Tensor(inputs).reshape(Shape(-1, 1)), Tensor(outputs).reshape(Shape(-1, 1)))
   }
 
+  def printRestoredNodesAndOperations(session: Session, input: Output, output: Output, weight: Output, bias: Output, loss: Output, placeholderW: Output, placeholderB: Output, gradientW: Output, gradientB: Output, trainOp: Op, assignVariableW: Op,  assignVariableB: Op): Unit ={
+    // ---------- PRINT RESTORED OUTPUT AND OPERATIONS ------------
+    println(" *" * 60)
+    myPrintln("Trained weight value: " + session.run(fetches = weight).scalar, 90)
+    myPrintln("Trained bias value: " + session.run(fetches = bias).scalar, 90)
+    myPrintln("" + input, 116)
+    myPrintln("" + output, 116)
+    myPrintln("" + weight, 116)
+    myPrintln("" + bias, 116)
+    myPrintln("" + loss, 116)
+    myPrintln("" + placeholderW, 116)
+    myPrintln("" + placeholderB, 116)
+    myPrintln("" + gradientW, 116)
+    myPrintln("" + gradientB, 116)
+
+    myPrintln("" + trainOp, 116)
+    myPrintln("" + assignVariableW, 116)
+    myPrintln("" + assignVariableB, 116)
+    println(" *" * 60)
+    // ---------------------------------------------
+
+    println("")
+  }
   def myPrintln(str: String, size: Int): Unit = {
     println(String.format("| %1$-" + size + "s |", str))
   }
